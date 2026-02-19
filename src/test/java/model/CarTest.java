51f22107cbd17ec0e19114f0bf801ac5cd3cdb46
Johnny Xiao <yuxiao9282@gmail.com>
2026-02-19 00:19:34 -0800

    repath test files

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,12 +1,16 @@
 
 package model;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import model.Car;
-import model.Level;
+import org.junit.Test;
 
-class CarTest {
+// ...existing code...
+
+import static org.junit.Assert.*;
+// ...existing code...
+
+public class CarTest {
+    // Public no-arg constructor for JUnit 4
+    public CarTest() {}
     // Example concrete Car subclass for testing
     static class TestCar extends Car {
         public TestCar(String make, String model, int hourlyRate) {
@@ -23,13 +27,13 @@ class CarTest {
     }
 
     @Test
-    void testCalculateAmount() {
+    public void testCalculateAmount() {
         Car car = new TestCar("Toyota", "Corolla", 10);
         assertEquals(50, car.calculateAmount(5));
     }
 
     @Test
-    void testGetMakeAndModel() {
+    public void testGetMakeAndModel() {
         Car car = new TestCar("Honda", "Civic", 8);
         assertEquals("Honda", car.getMake());
         assertEquals("Civic", car.getModel());
