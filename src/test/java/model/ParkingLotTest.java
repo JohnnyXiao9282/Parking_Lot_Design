1d07d47e0b9806bc19bd4f3652ab3d0a73683536
Julian-jh <julian151719@gmail.com>
2026-02-20 14:29:46 -0800

    parking lot test

--- src/test/java/model/ParkingLotTest.java
+++ src/test/java/model/ParkingLotTest.java
@@ -1,10 +1,25 @@
 package model;
 
 import org.junit.Test;
-
-
 import static org.junit.Assert.*;
 
 public class ParkingLotTest {
-    
+    @Test
+    public void testParkingLotInitialization() {
+        ParkingLot parkingLot = new ParkingLot();
+        assertNotNull(parkingLot.getFirstLevel());
+        assertNotNull(parkingLot.getSecondLevel());
+    }
+    @Test
+    public void testGetFirstLevel() {
+        ParkingLot parkingLot = new ParkingLot();
+        Level firstLevel = parkingLot.getFirstLevel();
+        assertTrue(firstLevel.getIsFirst());
+    }
+    @Test
+    public void testGetSecondLevel() {
+        ParkingLot parkingLot = new ParkingLot();
+        Level secondLevel = parkingLot.getSecondLevel();
+        assertFalse(secondLevel.getIsFirst());
+    }
 }
